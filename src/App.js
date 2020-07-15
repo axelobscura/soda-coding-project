3c6b8c36fa4dfc8074a5a8d3a39ce52943aef8c7
Axel Obscura <axosar@gmail.com>
2020-07-14 19:37:10 -0500

    Shuffling numbers

--- src/App.js
+++ src/App.js
@@ -10,16 +10,30 @@ const eldato = data.data.state.map(ely => {
   losDatos.push(ely);
 });
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
 function App() {
   const [count, setCount] = useState(0);
   const [textoBoton, setTextoBoton] = useState("START");
+
   const sum = () => {
     setCount(count + 1);
-    setTextoBoton("PAUSE")
+    setTextoBoton("PAUSE");
+    shuffle(losDatos);
   }
   return (
     <div className="App">
-      <h1>CODING PROJECT</h1>
+      <h1>CODING PROJECT {count}</h1>
       <Boton texto={textoBoton} sumar={sum} />
       {losDatos.map((cdato, index) => (
         <div className="contenedor" key={index}><Grid datos={cdato} /></div>
